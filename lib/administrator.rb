0e02c89c63feb5d6c4f995a775e77eab60b0cab6
Jackie <jonofre16@hotmail.com>
2018-03-05 13:20:56 -0800

    Instantiation of Admin passes, establishes the base data structures when instantiated is failing.

--- lib/administrator.rb
+++ lib/administrator.rb
@@ -4,3 +4,16 @@ require 'pry'
 require 'awesome_print'
 
 # require_relative 'room?'
+
+module Hotel
+  class Administrator
+
+    # attr_reader :list_of_rooms
+
+    def initialize
+
+    end
+
+  end
+
+end
--- specs/administrator_spec.rb
+++ specs/administrator_spec.rb
@@ -4,11 +4,20 @@ describe "Administrator class" do
 
   describe "Administrator instantiation" do
 
+    before do
+      @administrator = Hotel::Administrator.new
+    end
+
     it "is an instance of Administrator" do
-      administrator = Hotel::Administrator.new
-      administrator.must_be_kind_of Hotel::Administrator
+      @administrator.must_be_kind_of Hotel::Administrator
+    end
+
+    it "establishes the base data structures when instantiated" do
+      [:rooms, :reservations].each do |prop|
+        @administrator.must_respond_to prop
+      end
     end
 
   end
-  
+
 end
