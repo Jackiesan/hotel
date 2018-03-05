03791dbaec2879ee8efe75fab545b912c6c8f3f1
Jackie <jonofre16@hotmail.com>
2018-03-05 15:46:55 -0800

    added more assertions to #room_list_method

--- specs/administrator_spec.rb
+++ specs/administrator_spec.rb
@@ -39,10 +39,35 @@ describe "Administrator class" do
     it "accurately loads the information of the room" do
       @room_list.first.room_id.must_equal 1
       @room_list.last.room_id.must_equal 20
+      @room_list.length.must_equal 20
 
       all_costs = @room_list.all? { |room| room.cost_per_night == 200.00 }
       all_costs.must_equal true
     end
+
+    it "stores room id as an integer within range 1 and 20" do
+      all_room_ids =  @room_list.all? { |room| (room.room_id.class == Integer) && (room.room_id > 0 || room.room_id < 20) }
+      all_room_ids.must_equal true
+    end
+
+    it "stores cost per night as a float that is greater or equal to zero" do
+      all_costs_per_night =  @room_list.all? { |room| (room.cost_per_night.class == Float) && (room.cost_per_night >= 0) }
+      all_costs_per_night.must_equal true
+    end
+
+
+
   end
+
+  # describe "reserve_a_room method" do
+  #   before do
+  #     administrator = Hotel::Administrator.new
+  #     @first_reservation = administrator.reserve_a_room(1)
+  #   end
+  #
+  #   it ""
+  #
+  #
+  # end
 
 end
